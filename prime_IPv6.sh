e12b69dbd9c25176656f10089642f72107fb7b21
skakyn34 <46976565+skakyn34@users.noreply.github.com>
2019-05-13 06:03:59 +0000

    Update prime_IPv6.sh

--- prime_IPv6.sh
+++ prime_IPv6.sh
@@ -86,7 +86,7 @@ read wan
 					cd ~ &&
 					rm -fr primestone-cli primestoned primestone-tx prime_linux.zip &&
 					echo -e "${GREEN}2/5 Old Primestone wallet is deleted${NC}" &&
-					wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.0/prime_linux.zip &&
+					wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.1/prime_linux.zip &&
 					echo -e "${GREEN}3/5 Primestone wallet is downloaded${NC}" &&
 					unzip -o prime*.zip &&
 					sudo cp -fr primestone-cli primestoned /usr/bin/ &&
@@ -124,7 +124,7 @@ read wan
 					cd ~ &&
 					rm -fr primestone-cli primestoned primestone-tx prime_linux.zip &&
 					echo -e "${GREEN}2/5 Old Primestone wallet is deleted${NC}" &&
-					wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.0/prime_ubuntu_16.zip &&
+					wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.1/prime_ubuntu_16.zip &&
 					echo -e "${GREEN}3/5 Primestone wallet is downloaded${NC}" &&
 					unzip -o prime*.zip &&
 					sudo cp -fr primestone-cli primestoned /usr/bin/ &&
@@ -184,10 +184,10 @@ cd ~
 rm -fr prime*.zip
 rm -R -fr prime_linux
             if [ "$OS_version" -eq "1" ]; then
-                wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.0/prime_linux.zip
+                wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.1/prime_linux.zip
 		if [ $? -ne "0" ]; then echo "Failed to download primestoned binary" && exit 1; fi
             elif [ "$OS_version2" -eq "1" ]; then
-                wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.0/prime_ubuntu_16.zip
+                wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.2.1/prime_ubuntu_16.zip
 		if [ $? -ne "0" ]; then echo "Failed to download primestoned binary" && exit 1; fi
             fi
 # Manage coin daemon and configuration //

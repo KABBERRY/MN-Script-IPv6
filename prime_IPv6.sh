ebe11db19b5173d35e860ee51ba0a6c894cacaf3
Rebel36 <50796892+Rebel36@users.noreply.github.com>
2019-12-27 01:03:44 +0100

    Update prime_IPv6.sh

--- prime_IPv6.sh
+++ prime_IPv6.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-#:: Primestone team
+#:: Kabberry team
 #:: Copyright // 2019-01-01
-#:: Version: v2.4
+#:: Version: v2.5
 #:: Tested on Ubuntu 18.04 & Ubuntu 16.04
 cat << "PSC"
+
 
 
 
@@ -17,6 +18,7 @@ cat << "PSC"
  |_|    |_____/ \_____| |____(_)____/  |_____/|______|  |_|   \____/|_|     
                                                                             
                                                                             
+                                                                              
  
  
 PSC
@@ -27,10 +29,10 @@ NC='\033[0m'
 
 echo $(date)
 echo ""
-echo "Good day. This is automated cold masternode setup for Primestone project. Auto installer was tested on specific environment. Don't try to install masternode with undocumented operating system!"
+echo "Good day. This is automated cold masternode setup for Kabberry project. Auto installer was tested on specific environment. Don't try to install masternode with undocumented operating system!"
 echo ""
 echo "Installation content:"
-echo "primestone core v2.4"
+echo "kabberry core v2.5"
 echo
 echo "Setup can be launched"
 echo "Do you agree?"
@@ -80,28 +82,28 @@ read wan
 					read setorupd
 					if [ "$setorupd" = "u" ]; then
 					sudo systemctl stop primecore &&
-					echo -e "${GREEN}1/5 Primestone service is stopped${NC}" &&
+					echo -e "${GREEN}1/5 Kabberry service is stopped${NC}" &&
 					cd /usr/bin &&
-					sudo rm -fr primestone-cli primestoned &&
+					sudo rm -fr kabberry-cli kabberryd &&
 					cd ~ &&
-					rm -fr primestone-cli primestoned primestone-tx prime_linux.zip &&
-					echo -e "${GREEN}2/5 Old Primestone wallet is deleted${NC}" &&
-					wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.4.0/prime_linux.zip &&
-					echo -e "${GREEN}3/5 Primestone wallet is downloaded${NC}" &&
-					unzip -o prime*.zip &&
-					sudo cp -fr primestone-cli primestoned /usr/bin/ &&
-					rm -fr primestone-cli primestoned primestone-tx primestone-qt prime_linux.zip &&
+					rm -fr kabberry-cli kabberryd kabberry-tx kabberry_linux.zip &&
+					echo -e "${GREEN}2/5 Old Kabberry wallet is deleted${NC}" &&
+					wget https://github.com/KABBERRY/Kabberry-Coin/releases/download/v2.5.0/kabberry_linux.zip &&
+					echo -e "${GREEN}3/5 Kabberry wallet is downloaded${NC}" &&
+					unzip -o kabberry*.zip &&
+					sudo cp -fr kabberry-cli kabberryd /usr/bin/ &&
+					rm -fr kabberry-cli kabberryd kabberry-tx kabberry-qt kabberry_linux.zip &&
 					cd /usr/bin &&
-					chmod -R 755 primestone-cli primestoned &&
+					chmod -R 755 kabberry-cli kabberryd &&
 					cd ~ &&
-					echo -e "${GREEN}4/5 Primestone wallet is updated${NC}" &&
+					echo -e "${GREEN}4/5 Kabberry wallet is updated${NC}" &&
 					sudo systemctl start primecore &&
-					echo -e "${GREEN}5/5 Primestone service is started${NC}" &&
+					echo -e "${GREEN}5/5 Kabberry service is started${NC}" &&
 					echo -e "${GREEN}Update is full completed.${NC}" && exit 1; fi
 					if [ "$setorupd" = "i" ]; then
 					sudo systemctl stop primecore &&
 					echo "" &&
-					echo -e "${GREEN}Setup Primestone masternode started${NC}" &&
+					echo -e "${GREEN}Setup Kabberry masternode started${NC}" &&
 					sleep 5
 					else
 					echo "" &&
@@ -118,33 +120,34 @@ read wan
 					read setorupd
 					if [ "$setorupd" = "u" ]; then
 					sudo systemctl stop primecore &&
-					echo -e "${GREEN}1/5 Primestone service is stopped${NC}" &&
+					echo -e "${GREEN}1/5 Kabberry service is stopped${NC}" &&
 					cd /usr/bin &&
-					sudo rm -fr primestone-cli primestoned &&
+					sudo rm -fr kabberry-cli kabberryd &&
 					cd ~ &&
-					rm -fr primestone-cli primestoned primestone-tx prime_linux.zip &&
-					echo -e "${GREEN}2/5 Old Primestone wallet is deleted${NC}" &&
-					wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.4.0/prime_ubuntu_16.zip &&
-					echo -e "${GREEN}3/5 Primestone wallet is downloaded${NC}" &&
-					unzip -o prime*.zip &&
-					sudo cp -fr primestone-cli primestoned /usr/bin/ &&
-					rm -fr primestone-cli primestoned primestone-tx primestone-qt prime_ubuntu_16.zip &&
+					rm -fr kabberry-cli kabberryd kabberry-tx kabberry_linux.zip &&
+					echo -e "${GREEN}2/5 Old Kabberry wallet is deleted${NC}" &&
+					wget https://github.com/KABBERRY/Kabberry-Coin/releases/download/v2.5.0/kabberry_ubuntu_16.zip &&
+					echo -e "${GREEN}3/5 Kabberry wallet is downloaded${NC}" &&
+					unzip -o kabberry*.zip &&
+					sudo cp -fr kabberry-cli kabberryd /usr/bin/ &&
+					rm -fr kabberry-cli kabberryd kabberry-tx kabberry-qt kabberry_ubuntu_16.zip &&
 					cd /usr/bin &&
-					chmod -R 755 primestone-cli primestoned &&
+					chmod -R 755 kabberry-cli kabberryd &&
 					cd ~ &&
-					echo -e "${GREEN}4/5 Primestone wallet is updated${NC}" &&
+					echo -e "${GREEN}4/5 Kabberry wallet is updated${NC}" &&
 					sudo systemctl start primecore &&
-					echo -e "${GREEN}5/5 Primestone service is started${NC}" &&
+					echo -e "${GREEN}5/5 Kabberry service is started${NC}" &&
 					echo -e "${GREEN}Update is full completed.${NC}" && exit 1; fi
 					if [ "$setorupd" = "i" ]; then
 					sudo systemctl stop primecore &&
 					echo "" &&
-					echo -e "${GREEN}Setup Primestone masternode started${NC}" &&
+					echo -e "${GREEN}Setup Kabberry masternode started${NC}" &&
 					sleep 5
 					else
 					echo "" &&
 					echo -e "${RED}Sorry, we cannot continue${NC}" && exit 1;   fi
-            fi
+            
+			fi
 if [ "$OS_version" -eq "1" ]; then
 # Install dep. for Bionic //
         sudo add-apt-repository ppa:bitcoin/bitcoin -y
@@ -155,12 +158,13 @@ if [ "$OS_version" -eq "1" ]; then
 	if [ $? -ne "0" ]; then echo "Unable to install libdb dependencies" && exit 1; fi
         sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common libminiupnpc-dev libcrypto++-dev libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-thread-dev libssl-dev libssl-dev software-properties-common unzip libzmq3-dev ufw wget git python-openssl -y
 		if [ $? -ne "0" ]; then echo "Unable to install major dependencies" && exit 1; fi
-		sudo wget https://github.com/Primestonecoin/MN-Script-IPv4/raw/master/libs.zip
+		sudo wget https://github.com/KABBERRY/MN-Script-IPv4/raw/master/libs.zip
 		unzip -o libs.zip
 		sudo cp -fr libboost_filesystem.so.1.58.0 libboost_chrono.so.1.58.0 libboost_program_options.so.1.58.0 libboost_system.so.1.58.0 libboost_thread.so.1.58.0 libminiupnpc.so.10 libevent_core-2.0.so.5 libevent_pthreads-2.0.so.5 libevent-2.0.so.5 /usr/lib/
 		rm -fr libboost_filesystem.so.1.58.0 libboost_chrono.so.1.58.0 libboost_program_options.so.1.58.0 libboost_system.so.1.58.0 libboost_thread.so.1.58.0 libminiupnpc.so.10 libevent_core-2.0.so.5 libevent_pthreads-2.0.so.5 libevent-2.0.so.5 libs.zip
         if [ $? -ne "0" ]; then echo "Unable to install libboost dependencies" && exit 1; fi
         else
+# Install dep. for Xenial //		
         sudo add-apt-repository ppa:bitcoin/bitcoin -y
         if [ $? -ne "0" ]; then echo "Unable to add bitcoin dependencies" && exit 1; fi
         sudo apt-get update -y
@@ -172,36 +176,37 @@ if [ "$OS_version" -eq "1" ]; then
         sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-pthreads-2.0-5 libevent-dev bsdmainutils software-properties-common libminiupnpc-dev libcrypto++-dev libboost-all-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-thread-dev libssl-dev libssl-dev software-properties-common unzip libzmq3-dev ufw wget git python-openssl -y
         if [ $? -ne "0" ]; then echo "Unable to install major dependencies" && exit 1; fi
         fi
-# Download primestone sources //
+# Download Kabberry sources //
 echo ""
-echo -e "${GREEN}1/6 Downloading Primestone sources...${NC}" 
+echo -e "${GREEN}1/6 Downloading Kabberry sources...${NC}" 
 echo ""
 cd /usr/bin
-sudo rm -fr primestone-cli primestoned
+sudo rm -fr kabberry-cli kabberryd
 cd ~/.primestone
 rm -R -fr database .lock peers.dat blocks db.log masternode.conf primestone.conf zerocoin budget.dat debug.log mncache.dat primestoned.pid chainstate fee_estimates.dat mnpayments.dat sporks
 cd ~
-rm -fr prime*.zip
-rm -R -fr prime_linux
+rm -fr kabberry*.zip
+rm -R -fr kabberry_linux
+
             if [ "$OS_version" -eq "1" ]; then
-                wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.4.0/prime_linux.zip
-		if [ $? -ne "0" ]; then echo "Failed to download primestoned binary" && exit 1; fi
+                wget https://github.com/KABBERRY/Kabberry-Coin/releases/download/v2.5.0/kabberry_linux.zip
+		if [ $? -ne "0" ]; then echo "Failed to download kabberryd binary" && exit 1; fi
             elif [ "$OS_version2" -eq "1" ]; then
-                wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.4.0/prime_ubuntu_16.zip
-		if [ $? -ne "0" ]; then echo "Failed to download primestoned binary" && exit 1; fi
+                wget https://github.com/KABBERRY/Kabberry-Coin/releases/download/v2.5.0/kabberry_ubuntu_16.zip
+		if [ $? -ne "0" ]; then echo "Failed to download kabberryd binary" && exit 1; fi
             fi
 # Manage coin daemon and configuration //
-unzip -o prime*.zip
+unzip -o kabberry*.zip
 echo ""
-sudo cp -fr primestone-cli primestoned /usr/bin/
-rm -fr primestone-cli primestoned primestone-tx primestone-qt prime_linux.zip prime_ubuntu_16.zip
+sudo cp -fr kabberry-cli kabberryd /usr/bin/
+rm -fr kabberry-cli kabberryd kabberry-tx kabberry-qt kabberry_linux.zip kabberry_ubuntu_16.zip
 cd /usr/bin
-chmod -R 755 primestone-cli primestoned
+chmod -R 755 kabberry-cli kabberryd
 cd ~
 mkdir -p ~/.primestone/
 touch ~/.primestone/primestone.conf
 cat << EOF > ~/.primestone/primestone.conf
-rpcuser=primestoneuser
+rpcuser=kabberryuser
 rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
 txindex=1
 primestonestake=1
@@ -222,15 +227,15 @@ echo -e "${GREEN}2/6 Create primecore.service for systemd${NC}"
 echo ""
 echo \
 "[Unit]
-Description=Primestone Core Wallet daemon & service
+Description=Kabberry Core Wallet daemon & service
 After=network.target
 
 [Service]
 User=root
 Type=forking
-ExecStart=/usr/bin/primestoned -daemon -pid=$(echo $HOME)/.primestone/primestoned.pid --datadir=$(echo $HOME)/.primestone/
+ExecStart=/usr/bin/kabberryd -daemon -pid=$(echo $HOME)/.primestone/primestoned.pid --datadir=$(echo $HOME)/.primestone/
 PIDFile=$(echo $HOME)/.primestone/primestoned.pid
-ExecStop=/usr/bin/primestone-cli stop
+ExecStop=/usr/bin/kabberry-cli stop
 Restart=always
 RestartSec=3600
 TimeoutStopSec=60s
@@ -260,7 +265,7 @@ sudo echo \
 sudo mv /tmp/$(echo $real_user) /etc/sudoers.d/
 fi
 
-# Start primestone daemon, wait for wallet creation //
+# Start kabberry daemon, wait for wallet creation //
 sudo systemctl start primecore &&
 echo "" ; echo "Please wait for few minutes..."
 sleep 120 &
@@ -306,7 +311,7 @@ do
   printf "\b${sp:i++%${#sp}:1}"
 done
 cat << EOF > ~/.primestone/primestone.conf
-rpcuser=primestoneuser
+rpcuser=kabberryuser
 rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
 txindex=1
 primestonestake=1
@@ -329,7 +334,7 @@ echo -e "${GREEN}4/6 Update firewall rules${NC}"
 echo ""
 echo "Update firewall rules"
 sudo /usr/sbin/ufw limit ssh/tcp comment 'Rate limit for openssh server' 
-sudo /usr/sbin/ufw allow 34124/tcp comment 'Primestone Wallet daemon'
+sudo /usr/sbin/ufw allow 34124/tcp comment 'Kabberry Wallet daemon'
 sudo /usr/sbin/ufw --force enable
 echo ""
 
@@ -338,15 +343,15 @@ cd ~
 cd .primestone
 rm -R blocks chainstate
 echo ""
-echo -e "${GREEN}5/6 please wait, installation script downloads Primestone blockchain ${NC}"
+echo -e "${GREEN}5/6 please wait, installation script downloads Kabberry blockchain ${NC}"
 echo ""
-wget https://github.com/Primestonecoin/PrimeStone/releases/download/v2.4.0/primestone-blockchain.zip
+wget https://github.com/KABBERRY/Kabberry-Coin/releases/download/v2.4.0/primestone-blockchain.zip
 unzip -o primestone-blockchain.zip
 rm -f  primestone-blockchain.zip
 
 # Final start
 echo ""
-echo -e "${GREEN}6/6 Masternode config done, Primestone wallet installed - starting again${NC}"
+echo -e "${GREEN}6/6 Masternode config done, Kabberry wallet installed - starting again${NC}"
 echo ""
 sudo systemctl start primecore
 echo -e "${RED}The blockchain is syncing from scratch. You have to wait few hours to sync all the blocks!${NC}"
@@ -354,9 +359,9 @@ echo ""
 echo "Setup summary:"
 echo "Masternode privkey: $privkey"
 echo "Your external IPv6 addr: $wanipv6"
-echo "Installation log: ~/primestone_masternode_installation.log"
-echo "Primestone Core datadir: "$(echo $HOME/.primestone/)""
+echo "Installation log: ~/kabberry_masternode_installation.log"
+echo "Kabberry Core datadir: "$(echo $HOME/.primestone/)""
 echo ""
-echo -e "Need additional help? Please visit Primestone Discord channel: ${GREEN}https://discord.gg/D6KeMSA${NC}"
+echo -e "Need additional help? Please visit Kabberry Discord channel: ${GREEN}https://discord.gg/D6KeMSA${NC}"
 echo ""
-primestone-cli getinfo | grep blocks
+kabberry-cli getinfo | grep blocks
